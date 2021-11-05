ee148fd598904fc635df496503d8ce7c0e985dbe
FakeNDRUnknown <81918263+FakeNDRUnknown@users.noreply.github.com>
2021-11-05 22:04:45 +0100

    Delete Ndr GUI Beta English.lua

--- Ndr GUI Beta English.lua
+++
@@ -1,8 +0,0 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Update", Text = "Fuck Yamete SCAMMER"})
-  wait(7)
-  game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Oficial Script its here", Text = "https://discord.gg/TDhWt3UEz2"})
-repeat wait(15)
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Update", Text = "Fuck Yamete SCAMMER"})
-  wait(7)
-  game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Oficial Script its here", Text = "https://discord.gg/TDhWt3UEz2"})
-  until FuckOF
